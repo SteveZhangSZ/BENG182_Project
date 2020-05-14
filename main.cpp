19f0785628a33f95fcfaa23e22e68f1cfa4ae597
Steve Zhang <sz.stevezhang.sz@gmail.com>
2020-05-14 16:33:40 -0700

    modified:   .DS_Store 	modified:   frames.txt 	modified:   main 	modified:   main.cpp 	modified:   main.dSYM/Contents/Resources/DWARF/main

--- main.cpp
+++ main.cpp
@@ -20,19 +20,24 @@ int main(){
     //std::cout << totalLine[106] << totalLine[106 + 1] << totalLine[106 + 2] << '\n';
     //std::cout << totalLine[133] << totalLine[133 + 1] << totalLine[133 + 2] << '\n';
     std::ofstream output("frames.txt");
-    for(std::size_t pos = totalLine.find(line = "ATG", 0);pos != std::string::npos; pos = totalLine.find(line,pos+1)){
-        std::size_t first(pos);
-        for(std::size_t second(pos + 1),third(pos + 2); third < totalLine.size();first+=3, second+=3, third+=3){
+    for(std::size_t start = totalLine.find(line = "ATG", 0);start != std::string::npos; start = totalLine.find(line,start+1)){
+        //totalLine[start] == 'A' from "ATG"
+        std::size_t first(start);
+        for(std::size_t second(start + 1),third(start + 2); third < totalLine.size();first+=3, second+=3, third+=3){
+
+            output << totalLine[first] << totalLine[second] << totalLine[third]; //The sequence. The start and stop codons and everything in between.
             //These ifs look for stop codons
             if(totalLine[first] == 'T' && 
             ((totalLine[second] == 'A' && (totalLine[third] == 'A' || totalLine[third] == 'G')) || 
             (totalLine[second] == 'G' && totalLine[third] == 'A'))){
                 break;
             }
-            output << totalLine[first] << totalLine[second] << totalLine[third]; //The sequence
+            
         }
-        output << '\t' << pos << '\t' << first << '\t' << (pos % 3) << '\n';
+        //The char at totalLine[first],totalLine[first + 1],totalLine[first + 2] form a stop codon
+        output << '\t' << start << '\t' << (first + 3) << '\t' << (start % 3) << '\n';
         //Tab + start + tab + end + tab + frame number + new line
+        //std::cout << start << ' ';
     }
 }
 //clang++ -Wall -std=c++1z -stdlib=libc++ -g main.cpp -o main && ./main
