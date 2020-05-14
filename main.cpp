5e8fb24edd51409e0eed4029f515f80e5038fb3a
Steve Zhang <sz.stevezhang.sz@gmail.com>
2020-05-13 18:36:32 -0700

    modified:   .DS_Store 	new file:   FASTAinOneLine.txt 	modified:   frames.txt 	modified:   main 	modified:   main.cpp 	modified:   main.dSYM/Contents/Resources/DWARF/main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include <cstdint>
-#include <iostream>
+#include <iostream> //std::cout for print statements to debug stuff
 #include <fstream> //open files
 
 struct Frame{
-    //Unused Frame object
+    //Unused and probably unnecessary Frame object
     const std::size_t start, end, frame;
     constexpr Frame() : start{0}, end{0}, frame{0}{}
     constexpr Frame(std::size_t s, std::size_t e, std::size_t f) : start{s}, end{e}, frame{f}{}
@@ -22,19 +22,17 @@ int main(){
     std::ofstream output("frames.txt");
     for(std::size_t pos = totalLine.find(line = "ATG", 0);pos != std::string::npos; pos = totalLine.find(line,pos+1)){
         std::size_t first(pos);
-        for(std::size_t second(pos + 1),third(pos + 2); third < totalLine.size();third = (second = (first+=3) + 1) + 1){
+        for(std::size_t second(pos + 1),third(pos + 2); third < totalLine.size();first+=3, second+=3, third+=3){
             //These ifs look for stop codons
-            if(totalLine[first] == 'T'){
-                if(totalLine[second] == 'A' && (totalLine[third] == 'A' || totalLine[third] == 'G')){
-                    break;
-                }
-                else if(totalLine[second] == 'G' && totalLine[third] == 'A'){
-                    break;
-                }
+            if(totalLine[first] == 'T' && 
+            ((totalLine[second] == 'A' && (totalLine[third] == 'A' || totalLine[third] == 'G')) || 
+            (totalLine[second] == 'G' && totalLine[third] == 'A'))){
+                break;
             }
-            output << totalLine[first] << totalLine[second] << totalLine[third];
+            output << totalLine[first] << totalLine[second] << totalLine[third]; //The sequence
         }
-        output << '\t' << pos << '\t' << first << '\n';
-        //The sequence + tab + start + tab + end + new line
+        output << '\t' << pos << '\t' << first << '\t' << (pos % 3) << '\n';
+        //Tab + start + tab + end + tab + frame number + new line
     }
 }
+//clang++ -Wall -std=c++1z -stdlib=libc++ -g main.cpp -o main && ./main
